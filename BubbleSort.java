2e578a65919dfdcac0e970548409e7d8a528cb01
Kevin Do <54718041+kevin-dough@users.noreply.github.com>
2022-04-08 22:16:49 -0700

    calculator pythagorean and swaps + comparisons for sorts

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
 
-public class BubbleSort {
+public class BubbleSort{
+    
     public static void bubbleSort(ArrayList<Integer> arr) {
+        int swaps = 0;
+        int comparisons = 0;
         for (int i = 0; i < arr.size(); i++) {
             for (int j = 0; j < arr.size() - 1; j++) {
+                comparisons++;
                 if (arr.get(j) > arr.get(j + 1)) {
+                    swaps++;
                     int temp = arr.get(j);
                     arr.set(j, arr.get(j + 1));
                     arr.set(j + 1, temp);
                 }
             }
         }
+        System.out.println("Swaps: " + swaps);
+        System.out.println("Comparisons: " + comparisons);
     }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -2,10 +2,15 @@ import java.util.ArrayList;
 
 public class SelectionSort {
     public static void selectionSort(ArrayList<Integer> arr) {
+        int swaps = 0;
+        int comparisons = 0;
+
         for (int i = 0; i < arr.size(); i++) {
             int min = i;
             for (int j = i + 1; j < arr.size(); j++) {
+                comparisons++;
                 if (arr.get(j) < arr.get(min)) {
+                    swaps++;
                     min = j;
                 }
             }
@@ -13,5 +18,8 @@ public class SelectionSort {
             arr.set(i, arr.get(min));
             arr.set(min, temp);
         }
+
+        System.out.println("Swaps: " + swaps);
+        System.out.println("Comparisons: " + comparisons);
     }
 }
